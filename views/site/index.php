048564b9ae8414891be55f60c92f4fb8139c08c5
Saba <saba.ansari44647@gmail.com>
2024-11-18 21:55:06 +0330

    9th commit

--- views/site/index.php
+++ views/site/index.php
@@ -1,18 +1,39 @@
 <?php
 use yii\helpers\Html;
+use yii\bootstrap5\Alert;
 
 $this->title = 'Home';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-<h1>Welcome!</h1>
-<?php if (Yii::$app->user->isGuest): ?>
-    <?= Html::a('Login', ['login'], ['class' => 'btn btn-primary']) ?>
-<?php else: ?>
-    <?php $user = Yii::$app->user->identity; ?>
-    <p>Hello, <?= Html::encode($user->username) ?>!</p>
-    <?php if ($user->role === 'customer'): ?>
-        <?= Html::a('View Menu', ['menu/index'], ['class' => 'btn btn-info']) ?>
-    <?php elseif ($user->role === 'restaurant'): ?>
-        <?= Html::a('Restaurant Panel', ['restaurant/index'], ['class' => 'btn btn-info']) ?>
-    <?php endif; ?>
-    <?= Html::a('Logout', ['site/logout'], ['class' => 'btn btn-danger', 'data-method' => 'post']) ?>
-<?php endif; ?>
+<div class="site-index">
+    <div class="jumbotron text-center bg-light p-5 rounded">
+        <h1 class="display-4"><?= Html::encode($this->title) ?></h1>
+        <p class="lead">Welcome to our platform!</p>
+    </div>
+
+    <div class="body-content">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <?php if (Yii::$app->user->isGuest): ?>
+                    <div class="text-center mt-4">
+                        <p class="lead">Join us to access exciting features!</p>
+                        <?= Html::a('Login', ['login'], ['class' => 'btn btn-primary btn-lg']) ?>
+                    </div>
+                <?php else: ?>
+                    <?php $user = Yii::$app->user->identity; ?>
+                    <div class="text-center mt-4">
+                        <p class="lead">Hello, <strong><?= Html::encode($user->username) ?></strong>!</p>
+                    </div>
+                    <div class="text-center mt-4">
+                        <?php if ($user->role === 'customer'): ?>
+                            <?= Html::a('View Menu', ['menu/index'], ['class' => 'btn btn-info btn-lg me-2']) ?>
+                        <?php elseif ($user->role === 'restaurant'): ?>
+                            <?= Html::a('Restaurant Panel', ['restaurant/index'], ['class' => 'btn btn-info btn-lg me-2']) ?>
+                        <?php endif; ?>
+                        <?= Html::a('Logout', ['site/logout'], ['class' => 'btn btn-danger btn-lg', 'data-method' => 'post']) ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
--- views/site/login.php
+++ views/site/login.php
@@ -3,15 +3,42 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 $this->title = 'Login';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
+<div class="site-login">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card shadow-lg">
+                <div class="card-header bg-primary text-white text-center">
+                    <h3><?= Html::encode($this->title) ?></h3>
+                    <p>Please fill out the following fields to login:</p>
+                </div>
+                <div class="card-body">
+                    <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-<h1>Login</h1>
-<?php $form = ActiveForm::begin(); ?>
-<?= $form->field($model, 'username') ?>
-<?= $form->field($model, 'password')->passwordInput() ?>
-<?= $form->field($model, 'rememberMe')->checkbox() ?>
-<div class="form-group">
-    <?= Html::submitButton('Login', ['class' => 'btn btn-primary']) ?>
+                    <?= $form->field($model, 'username', [
+                        'inputOptions' => ['class' => 'form-control', 'placeholder' => 'Enter your username'],
+                    ])->label(false) ?>
+
+                    <?= $form->field($model, 'password', [
+                        'inputOptions' => ['class' => 'form-control', 'placeholder' => 'Enter your password'],
+                    ])->passwordInput()->label(false) ?>
+
+                    <?= $form->field($model, 'rememberMe')->checkbox([
+                        'template' => "<div class=\"form-check\">{input} {label}</div>\n{error}",
+                    ]) ?>
+
+                    <div class="form-group text-center">
+                        <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-lg w-100']) ?>
+                    </div>
+
+                    <div class="text-center mt-3">
+                        <p>Don't have an account? <?= Html::a('Register here', ['register'], ['class' => 'text-decoration-underline']) ?></p>
+                    </div>
+
+                    <?php ActiveForm::end(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-<p>Don't have an account? <?= Html::a('Register', ['register']) ?></p>
-<?php ActiveForm::end(); ?>
