8e4436e6ffe175ba445467310f4687f8a326729a
Saba <saba.ansari44647@gmail.com>
2024-12-13 20:21:19 +0330

    24th commit

--- views/pizza-order/view.php
+++ views/pizza-order/view.php
@@ -1,19 +1,17 @@
 <?php
+/* @var $this yii\web\View */
+/* @var $pizzaOrder app\models\PizzaOrder */
 
 use yii\helpers\Html;
 
-/* @var $this yii\web\View */
-/* @var $model app\models\PizzaOrder */
-
 $this->title = 'Order Details';
 ?>
-
 <div class="pizza-order-view">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p><strong>Bread Types:</strong> <?= implode(', ', $model->bread_types) ?></p>
-    <p><strong>Sausage Types:</strong> <?= implode(', ', $model->sausage_types) ?></p>
-    <p><strong>Toppings:</strong> <?= implode(', ', $model->toppings) ?></p>
+    <p><strong>Bread Types:</strong> <?= implode(', ', $pizzaOrder->bread_types) ?></p>
+    <p><strong>Sausage Types:</strong> <?= implode(', ', $pizzaOrder->sausage_types) ?></p>
+    <p><strong>Toppings:</strong> <?= implode(', ', $pizzaOrder->toppings) ?></p>
 
-    <?= Html::a('Back to Order Form', ['create', 'restaurant_id' => $model->restaurant_id], ['class' => 'btn btn-primary']) ?>
-    </div>
+    <?= Html::a('Back to Order Form', ['create', 'restaurant_id' => $pizzaOrder->restaurant_id], ['class' => 'btn btn-primary']) ?>
+</div>
