b13af3df7d3bf93383de4295ee765106356fe330
Saba <saba.ansari44647@gmail.com>
2024-12-15 00:13:35 +0330

    25th commit

--- views/pizza-order/view.php
+++ views/pizza-order/view.php
@@ -3,7 +3,6 @@
 /* @var $pizzaOrder app\models\PizzaOrder */
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
 
 $this->title = 'Order Details';
 ?>
@@ -14,19 +13,5 @@ $this->title = 'Order Details';
     <p><strong>Sausage Types:</strong> <?= implode(', ', $pizzaOrder->sausage_types) ?></p>
     <p><strong>Toppings:</strong> <?= implode(', ', $pizzaOrder->toppings) ?></p>
 
-    <h3>Enter Prices</h3>
-    
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($pizzaOrder, 'bread_price')->textInput(['type' => 'number', 'step' => '0.01']) ?>
-    <?= $form->field($pizzaOrder, 'sausage_price')->textInput(['type' => 'number', 'step' => '0.01']) ?>
-    <?= $form->field($pizzaOrder, 'toppings_price')->textInput(['type' => 'number', 'step' => '0.01']) ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Save Prices', ['class' => 'btn btn-success']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
     <?= Html::a('Back to Order Form', ['restaurant/view', 'id' => $pizzaOrder->restaurant_id], ['class' => 'btn btn-primary']) ?>
-</div>
+    </div>
